da17ba0fd4eb3696854e1c09756d1899189b8136
Itai Koren <34794316+itai590@users.noreply.github.com>
2026-05-09 19:02:03 +0300

    feat: add script to run backend and frontend tests sequentially

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+cd backend
+echo "🧪 Running backend tests..."
+npm test
+
+echo "==============================="
+
+cd ../frontend
+echo "🧪 Running frontend tests..."
+npm test
